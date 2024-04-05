7385df2eccf21daf8c47c12812e4a64ab5ffb62f
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-04-05 09:09:44 +0300

    add first http request

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,11 @@ import { PORT } from "./config.js";
 
 const app = express();
 
+app.get('/', (req, res) =>{
+    console.log(req)
+    return res.status(200).send("welcome")
+})
+
 app.listen(PORT, () => {
   console.log(`server is running on port: ${PORT}`);
 });
