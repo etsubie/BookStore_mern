53761daaa538f20544b6ec0a3aadae6b2a2a56c8
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-04-05 08:57:58 +0300

    first commit

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import { createStore, compose, applyMiddleware } from 'redux'; 
+// import {thunk} from 'redux-thunk';
+// import { Provider } from 'react-redux';
+
+import reducers from './reducers';
+import App from "./App";
+
+const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    {/* <Provider store={store}> */}
+      <App />
+    {/* </Provider> */}
+  </React.StrictMode>
+);
