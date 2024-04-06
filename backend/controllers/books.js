d00818c7da53750a83998e073179f31e514efe3f
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-04-06 13:59:00 +0300

    add crud opertaion

--- backend/controllers/books.js
+++ backend/controllers/books.js
@@ -1,9 +1,10 @@
 import bookModel from "../models/book.js";
 
 export const createBooks = async (req, res) => {
-  const book = req.body;
-  const newBook = new bookModel(book);
   try {
+    const book = req.body;
+    const newBook = new bookModel(book);
+
     await newBook.save();
     res.status(201).json(newBook);
   } catch (error) {
@@ -16,7 +17,53 @@ export const fetchBooks = async (req, res) => {
   try {
     const book = req.body;
     const books = await bookModel.find(book);
-    res.json(books);
+    res.status(200).json({ count: books.length, data: books });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const fecthBook = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const Book = await bookModel.findById(id);
+    if (!Book) {
+      return res.status(404).send({ message: "Book Not Found" });
+    }
+    res.status(200).json(Book);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const updateBooks = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const book = req.body;
+
+    const updatedBook = await bookModel.findByIdAndUpdate(id, book, {
+      new: true,
+    });
+    if (!updatedBook) {
+      return res.status(404).json({ message: " Book Not Found!" });
+    }
+    res.status(200).json("Book Updated Succesfuly");
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Server Error" });
+  }
+};
+
+export const deleteBooks = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedBook = await bookModel.findByIdAndDelete(id);
+    if (!deletedBook) {
+      return res.status(404).json({ message: "Book Not Found!" });
+    }
+    res.status(200).json("Book Deleted Successfuly");
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: "Server Error" });
--- backend/routes/books.js
+++ backend/routes/books.js
@@ -1,9 +1,18 @@
 import express from "express";
-import { createBooks, fetchBooks } from "../controllers/books.js";
+import {
+  createBooks,
+  deleteBooks,
+  fecthBook,
+  fetchBooks,
+  updateBooks,
+} from "../controllers/books.js";
 
 const bookRouter = express.Router();
 
 bookRouter.post("/create", createBooks);
 bookRouter.get("/", fetchBooks);
+bookRouter.get("/:id", fecthBook);
+bookRouter.patch("/edit/:id", updateBooks);
+bookRouter.delete("/delete/:id", deleteBooks);
 
 export default bookRouter;
