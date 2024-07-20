eb8d47693e1ecd5a90e0b9e090a36d14f05dc4aa
Etsubdink Yetwale <etsubie2124@gmail.com>
2024-07-20 16:02:03 +0300

    updated

--- frontend/src/api/books.js
+++ frontend/src/api/books.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "https://bookstore-d3sv.onrender.com/" });
+const API = axios.create({ baseURL: "http://bookstore-d3sv.onrender.com/" });
 
 export const createBook = (book) => API.post("/create", book);
 export const fetchBooks = () => API.get("/");
